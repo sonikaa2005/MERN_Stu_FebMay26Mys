53a9dd71ed797530cd00bf3b56428c52e3cd6160
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-03 12:00:31 +0530

    Comaprision operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+//Comparision operators
+
+console.log("Comparison operators");
+let x=2, y='3';
+console.log("x==y: ",x==y); //Loose equality
+console.log("x===y: ",x===y); //Strict equality
+console.log("x!=y: ",x!=y); //loose inequality
+console.log("x!==y: ",x!==y); //Strict inequality
+console.log("x>3: ", x>3);
+console.log("x<3: ", x<3);
+console.log("x>=3: ", x>=3);
+console.log("x<=3: ", x<=3);
