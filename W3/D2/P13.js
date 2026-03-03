4c79b75e174ee81996a51635215e8d8f008d1a77
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-03 15:40:52 +0530

    Function scope

---
+++ W3/D2/P13.js
@@ -0,0 +1,25 @@
+//Function scope
+function scope(){
+    var insideVar = 10;
+    let insideLet = 20;
+    const insideConst = 30;
+    console.log(insideVar);
+    console.log(insideLet);
+    console.log(insideConst);
+}
+scope();
+
+console.log(insideVar); //function scoped
+    console.log(insideLet); // block scoped
+    console.log(insideConst); // block scoped
+
+
+function varFunctionScoped(){
+    if(true){
+        var x = 40;
+        let y = 50;
+    }
+    console.log("x: ",x);
+    console.log("y: ",y);
+}
+varFunctionScoped();
