6ce9e6dee163df6fcad96dc75e80f33fb5a60f8a
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-03 12:21:36 +0530

    Function Declaration

---
+++ W3/D2/P5.js
@@ -0,0 +1,9 @@
+// Function Declaration
+// function greet(name);  "this is only in the case of c pgming"
+greet("Sonika");  //hoisting
+// Function Declaration/Definition
+function greet(name){
+    console.log("Hello,"+name+" !");
+}
+// function call
+// greet("Sonika");
