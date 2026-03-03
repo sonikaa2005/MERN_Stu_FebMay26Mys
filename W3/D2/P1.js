ed62b9c89b45d61f8466825eb698235a2f626537
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-03 11:59:12 +0530

    Arithmatic operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+// Arithmatic operators
+console.log("Arithmatic operators");
+let a = 2, b = 3;
+let c = 0;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a+b",a%b);//modulus
+console.log("a**b",a**b);//exponential
+console.log("a++",a++); //post-increment
+console.log("++a",++a); //pre-increment
+console.log("a--",a--); //post-decrement
+console.log("--a",--a); //pre-decrement
