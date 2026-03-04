bfe460f9b6cacfc8184f9a4e11ab846b1f877ec2
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-04 13:25:19 +0530

    Basics of objects

---
+++ W3/D3/P5.js
@@ -0,0 +1,9 @@
+//Basics of Objects
+const person = {
+    name:"Rahul",
+    age:30,
+    isStudent: false
+};
+console.log("Person",person);
+console.log("Name",person.name);//dot notation
+console.log("Age",person["age"]);//bracket notation
