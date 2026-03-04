4f3ee83f44a8acd5be7b8ae67a3b4389675ec783
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-04 13:40:06 +0530

    basic objet

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -7,3 +7,20 @@ const person = {
 console.log("Person",person);
 console.log("Name",person.name);//dot notation
 console.log("Age",person["age"]);//bracket notation
+
+//Add a new property
+person.city = "Mysore";
+console.log("Person",person);
+
+//modify
+person.age = 31;
+//dlete
+delete person.isStudent;
+console.log("Person",person);
+
+//object constructor
+const car = new Object();
+car.make = "Audi";
+car.model = "A4";
+car.year = 2026
+console.log("Car",car);
