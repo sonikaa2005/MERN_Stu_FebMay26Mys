3021925d272b0918b19e0ede511f22ec1151c185
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-04 13:22:59 +0530

    While, do loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,16 @@
+//while loop
+console.log("while loop");
+
+let count = 0;
+while(count<5){
+    console.log("count: ",count);
+    count++;
+}
+
+ //do while
+
+ let value = 10;
+ do{
+    console.log("Value :",value);
+    value++;
+ }while(value < 3);
