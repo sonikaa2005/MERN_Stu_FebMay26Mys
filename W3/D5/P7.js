eff8ab5661c1799e573f57a7ec6b3c0aa85c2478
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-06 16:45:06 +0530

    Event bubbling & stopPropogation

---
+++ W3/D5/P7.js
@@ -0,0 +1,14 @@
+const outer=document.getElementById("outer");
+const inner=document.getElementById("inner");
+const innerButton=document.getElementById("innerButton");
+outer.addEventListener("click",function(){
+    console.log("outer div clicked");
+});
+inner.addEventListener("click",function(){
+    console.log("inner div clicked");
+});
+innerButton.addEventListener("click",function(event){
+    console.log("Inner button clicked");
+    event.stopPropagation();
+    console.log("Propogation stopped at button");
+});
