2f5b591565ac2654e81c8cb48e8e2cd329a508a8
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-09 17:42:15 +0530

    Dom introduction

---
+++ W4/D1/P7.js
@@ -0,0 +1,36 @@
+//DOM
+// console.log("Document Object:",document);
+// console.log("Page title:",document.title);
+
+//const heading = document.getElementById("mainHeding");
+// console.log("Heading text",heading.textContent);
+
+// getElementsByClassName
+const info = document.getElementsByClassName("info");
+const run = document.getElementById("run");
+
+//getElementsByTagName
+const spanTag = document.getElementsByTagName("span");
+
+run.addEventListener("click",function(){
+    for(let i=0;i<info.length;i++){
+        console.log(info[i].textContent);
+        info[i].style.color = "blue";
+    }
+    for(let i=0;i<spanTag.length;i++){
+        spanTag[i].style.backgroundColor = "lightgreen";
+    }
+    // Query selector:returns the first element
+//matching a css selector
+ const mainFirstHeading = document.querySelector(".mainHeading"); //.=id, #=class
+ mainFirstHeading.style.color = "red";
+});
+
+//querry selector all: returns all elements matchimg the selector
+
+const tasks = document.querySelectorAll(".task");
+//task.style.color = "purple";
+tasks.forEach(function(task){
+    task.style.color = "purple";
+});
+ 
