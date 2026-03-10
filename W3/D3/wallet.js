1002c4e273fec2202e57cfaf590140b83573eb9e
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-10 09:04:58 +0530

    Wallet with 'this' keyword

---
+++ W3/D3/wallet.js
@@ -0,0 +1,49 @@
+let wallet = {
+    owner: "Sonika",
+    balance: 0,
+    lastTransaction: null,
+
+    deposit: function(amount) {
+
+        if (typeof amount !== "number" || amount <= 0) {
+            console.log("Invalid amount");
+            return;
+        }
+
+        this.balance += amount;
+
+        this.lastTransaction = {
+            type: "DEPOSIT",
+            amount: amount,
+            balanceAfter: this.balance
+        };
+    },
+
+    withdraw: function(amount) {
+
+        if (typeof amount !== "number" || amount <= 0) {
+            console.log("Invalid amount");
+            return;
+        }
+
+        if (amount > this.balance) {
+            console.log("Insufficient balance");
+            return;
+        }
+
+        this.balance -= amount;
+
+        this.lastTransaction = {
+            type: "WITHDRAW",
+            amount: amount,
+            balanceAfter: this.balance
+        };
+    }
+};
+
+
+// Test
+wallet.deposit(500);
+wallet.withdraw(200);
+
+console.log(wallet);
