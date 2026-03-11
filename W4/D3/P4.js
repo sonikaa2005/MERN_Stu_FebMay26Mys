923b5a8f6b422dee5ed207ac1cd89ad4c1e455c5
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-11 15:43:51 +0530

    storing and retriving JSON data

--- W4/D3/P4.js
+++ W4/D3/P4.js
@@ -16,9 +16,14 @@ document.getElementById("readBtn").addEventListener("click", function () {
     // const user = localStorage.getItem("user");
     // localStorage.setItem("userProfile", JSON.stringify(user));
     // jsonOutput.textContent = "user object read as string to localStorage";
-
-    const up = localStorage.getItem("userProfile");
-    console.log(JSON.parse(up));
-    console.log(up);
-    jsonOutput.textContent="userProfile"+ up;
+    try {
+        const up = localStorage.getItem("userProfile");
+        console.log(JSON.parse(up));
+        console.log(up);
+        jsonOutput.textContent = "userProfile" + up;
+    }
+    catch(error){
+        jsonOutput.textContent= "JASON parsing failed";
+    }
+    
 });
