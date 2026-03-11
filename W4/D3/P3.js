5358902c273d15578fff85366b5b436958b5eee7
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-11 12:36:34 +0530

    Saving and Reading the Session Storage

---
+++ W4/D3/P3.js
@@ -0,0 +1,29 @@
+const themeInput = document.getElementById("themeInput");
+const output = document.getElementById("output");
+
+document.getElementById("saveBtn").addEventListener("click", function () {
+    sessionStorage.setItem("theme", themeInput.value);
+    sessionStorage.setItem("userName", "Anu");
+    sessionStorage.setItem("loggedIn", "true");
+    console.log("Saved theme: ", themeInput.value);
+    output.textContent = "Saved theme to sessionStorage";
+    output.style.color = "green";
+});
+
+document.getElementById("readBtn").addEventListener("click", function () {
+    const theme = sessionStorage.getItem("theme");
+    output.textContent = "theme is: " + theme;
+    output.style.color = "green";
+
+});
+document.getElementById("removeBtn").addEventListener("click", function () {
+    sessionStorage.removeItem("loggedIn");
+    output.textContent = "Removed 'loggedIn' ";
+    output.style.color = "green";
+});
+
+document.getElementById("clearBtn").addEventListener("click", function () {
+    sessionStorage.clear();
+    output.textContent = "Deleted sessionStorage";
+    output.style.color = "red";
+});
