8c1eda7860ac6df944298890be0f76f5492184e4
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-18 11:41:33 +0530

    await with non-promise values

---
+++ W5/D3/P6.js
@@ -0,0 +1,18 @@
+// await with non-promise values
+
+function getReadyValue(){
+    return 25;
+}
+
+function getDelayedValue(){
+    return Promise.resolve(75);
+}
+
+async function showValues() {
+    const readyValue = await getReadyValue();
+    const delayedValue = await getDelayedValue();
+
+    console.log("Ready Value: ",readyValue);
+    console.log("Delayed value: ",delayedValue);
+}
+showValues();
