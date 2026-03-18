dbc5c1e106a144dd8231fe8e68fc478e5e6c2ddd
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-18 10:40:25 +0530

    Handling errors with try/catch

---
+++ W5/D3/P3.js
@@ -0,0 +1,28 @@
+//Handling errors with try/catch
+
+function loadCustomerProfile(){
+    return new Promise(function(resolve,reject){
+        const isServiceAvailable = true;
+
+        if(isServiceAvailable){
+            resolve("Success! Customer profile loaded.");
+        }
+        else{
+            reject("Unsuccessfull!! Customer profile unavailable.");
+        }
+    });
+}
+
+async function showCustomerProfile(){
+    try{
+        const message = await loadCustomerProfile();
+        console.log(message);
+    }
+    catch(error){
+        console.error("Error:",error);
+    }
+   
+}
+
+showCustomerProfile();
+
