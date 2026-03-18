6427c4b3ccc9d1c0e70b035f967e69ea078967eb
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-18 12:25:24 +0530

    try catch finally with async/await

---
+++ W5/D3/P8.js
@@ -0,0 +1,31 @@
+//try catch finally with async/await
+
+function processPayment(isSucceeded){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            if(isSucceeded){
+                resolve("Payment processed successfully.");
+            }
+        else{
+            resolve("Payment couldn't processed successfully");
+            }
+        })
+    })
+}
+async function runPaymentFlow(isSucceeded) {
+    try{
+        const result = await processPayment(isSucceeded);
+        console.log("Success",result);
+    }
+    catch(error){
+        console.log("Failure:",error);
+    }
+    finally{
+        console.log("Payment flow completed.");
+    }
+    
+}
+
+runPaymentFlow(true).then(function(){
+    return runPaymentFlow(true);
+});
