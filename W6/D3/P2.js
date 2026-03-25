9599ac3c911d93b9cd227fdc5f5051df2e9b132b
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-25 11:55:51 +0530

    Reading & writing files synchronously(

--- W6/D3/P2.js
+++ W6/D3/P2.js
@@ -8,11 +8,19 @@ const filePath = path.join(__dirname,"sync-note.txt");//function of the path mod
 //syntax for function usage in module
 //moduleName.functionName()
 
-fs.writeFileSync(filePath,"This file was written usingwriteFileSync().\nSynchronous operation block");
-
-console.log("File written synchronosly.")
+//fs.writeFileSync(filePath,"This file was written usingwriteFileSync().\nSynchronous operation block");
+//Append to a file
+fs.appendFileSync(filePath, "New text added using fs.appendFile.",function(error){
+    if(error){
+        console.log("Append error:",error.message);
+        return;
+      }
+      console.log("File content appended");
+    }
+);
+console.log("File written synchronosly.");
 
 const content = fs.readFileSync(filePath,"utf-8");
 
 console.log("File read synchronously.");
-console.log(content);
+console.log("File content:\n",content);
