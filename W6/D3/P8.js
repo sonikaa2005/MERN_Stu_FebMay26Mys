ea968272b76538d5a84be07e75e21b5275eccbac
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-26 09:20:31 +0530

    Allocating buffers

---
+++ W6/D3/P8.js
@@ -0,0 +1,21 @@
+//Allocating buffers
+
+const emptyBuffer = Buffer.alloc(8);
+console.log("Contents of emptyBuffer: ",emptyBuffer);
+console.log("Allocated buffer bytes:",[...emptyBuffer]);
+
+const textBuffer = Buffer.from([65,66,67]);
+console.log("Buffer from Byte array:",textBuffer);
+
+//Buffer.write() writes text into the buffer
+
+const buffer = Buffer.alloc(20);
+const bytesWritten = buffer.write("HelloWorld");//from
+console.log("Bytes written: ",bytesWritten);
+
+//Subarray
+const firstSlice = buffer.subarray(3,6);
+console.log("First slice as bytes: ",[...firstSlice]);
+
+//decode the bytes into text: toString()
+console.log("Decode firstslice of buffer:",firstSlice.toString("utf-8"));//firstSlice.subarray/ Buffer.subarray.toString
