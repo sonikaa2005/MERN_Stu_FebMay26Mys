9078789918587892d6b279bc09dfb6d5bfe28b0c
sonikaa2005 <sonikagowda974@gmail.com>
2026-03-31 15:12:55 +0530

    Sending JSON response using express

---
+++ W7/D1/P6.js
@@ -0,0 +1,25 @@
+//Sending JSON response using express
+
+const express=require("express");
+
+const app=express();
+
+app.get("/api/status",function(req,res){
+    res.json({
+        success:true,
+        message:"Express API is working"
+    });
+});
+
+app.get("/api/error",function(req,res){
+    //res.JSON() automatically serailizes the object and sets the JSON content type
+    res.status(404).JSON({
+        success:false,
+        message:"Requested resource is not found"
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
+//http://localhost:4000/api/status
