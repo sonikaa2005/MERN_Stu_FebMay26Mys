162afad6903018b8e7c89201ab202f3cde5d437e
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-01 10:30:52 +0530

    Handling Synchronous errors

---
+++ W7/D3/P1.js
@@ -0,0 +1,40 @@
+//Handling Synchronous errors
+
+const express = require("express");
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("Visit /check?id=10 or /check without id");
+});
+
+app.get("/check",function(req,res,next){
+    try{ // Synchronous validation check
+        if(!req.query.id){
+           throw new Error("Query parameter 'id' is required"); 
+        }
+        res.json({
+            success:true,
+            id:req.query.id
+        });
+    }
+    catch(error){
+        //Forward the error to centralized error middleware
+        next(error); //pass the error to the error handling middleware
+
+    }
+});
+
+//centralized error handling middleware
+app.use(function(error,req,res,next){
+    res.status(400).json({ // 400 request error
+        success:false,
+        message:error.message
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
+
+//http://localhost:4000
+//http://localhost:4000/check?id=10
