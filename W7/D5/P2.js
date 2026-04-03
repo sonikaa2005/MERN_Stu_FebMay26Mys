a7221170ae538f5f475e16e3292be5ef3954ec77
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-03 10:31:19 +0530

    Managing sessions with express-session

---
+++ W7/D5/P2.js
@@ -0,0 +1,43 @@
+// Managing sessions with express-session
+
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+
+app.use(session({
+    secret:"MySecretKey",
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:60*60*1000
+    }
+}));
+//API 1
+app.get("/login",function(req,res){
+    //after succssfull login details
+    req.session.user = {
+        id:201,
+        username: "Sonika",
+        role: "student"
+    };
+    res.send("Session details stored after login.");
+});
+
+//API 2
+app.get("/profile",function(req,res){
+    if(!req.session.user){
+        return res.status(401).json({ // error senario
+            success:false,
+            message:"No active login session found."
+        });
+    }
+    res.json({
+        success: true,
+        sessionUser: req.session.user
+    });
+});
+
+app.listen(4000, function () {
+    console.log("Express-session demo server running @ http://localhost:4000");
+});
