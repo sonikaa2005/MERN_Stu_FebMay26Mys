bf5b1e2f3935c39ae2eb8e4ccdf1816c16fd7d47
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-08 11:39:44 +0530

    Checks the permission for the req and allows it or rejects it.

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/roleMiddleware.js
@@ -0,0 +1,16 @@
+//Checks the permission for the req and allows it or rejects it
+
+const CustomError = require("../utils/customError");
+
+function roleMiddleware(...allowedRoles) {//allowedroles as a rest operators
+    return(req, res, next) => {
+        if (!req.user) {
+            return next(new CustomError("User info not found.", 401));
+        }
+        if(!allowedRoles.includes(req.user.role)) {
+            return next(new CustomError("Forbidden: You do not have access to this resource.",401));
+        }
+        next();
+    };
+}
+module.exports = roleMiddleware;
