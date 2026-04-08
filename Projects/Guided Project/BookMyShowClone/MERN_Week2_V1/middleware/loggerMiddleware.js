c61d1b8322c50845db64693c28b54f8fc4963f24
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-08 10:30:03 +0530

    Middleware for req log

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/loggerMiddleware.js
@@ -0,0 +1,8 @@
+//Middleware for req log
+
+function loggerMiddleware(req,res,next){
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+}
+module.exports = loggerMiddleware;
+//Task:write the log to a file along with timestamp with both req and res
