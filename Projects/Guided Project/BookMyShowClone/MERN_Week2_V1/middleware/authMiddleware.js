b3e865b2ad4f12e66d8496cec21794b9ff31a376
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-08 11:30:14 +0530

    Middleware to create and/or verify JWT token

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+//Middleware to create and/or verify JWT token
+
+const jwt = require("jsonwebtoken");
+const CustomError = require("../utils/customError");//"../(get out of the folder first)"
+
+const JWT_SECRET = "Thumbnail"; //secret key
+
+function authMiddleware(req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
+        const tokenFromHeader = authHeader && authHeader.startsWith("Bearer") ? authHeader.split(" ")[1] : null;
+
+        const token = tokenFromHeader || req.cookies.token;
+
+        if (!token) {
+            return next(new CustomError("Access denied. Token not provided", 401));
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch (error) {
+        return next(new CustomError("Invalid or expired token", 401));
+    }
+}
+
+module.exports = {
+    authMiddleware,
+    JWT_SECRET,
+};
