f8d8cd7b5521c0736eaa12385f54ea2ad774b5ac
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-08 12:31:04 +0530

    Custom error class created for error handling.

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/utils/customError.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/utils/customError.js
@@ -1,9 +1,9 @@
 //Custom error class created for error handling
-class CustomError extends Error{
+class customError extends Error{
     constructor(message,statusCode){
         super(message);//invoke the constructor of the super class
         this.statusCode = statusCode;
     }
 }
 
-module.exports = CustomError;
+module.exports = customError;
