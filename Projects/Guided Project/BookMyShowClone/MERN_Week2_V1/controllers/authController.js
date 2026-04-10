b938a8188dce49d975949db1ea47d921380ab41f
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-10 10:29:47 +0530

    User Login, profile fetch and logout of user functionality created

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
@@ -1,87 +1,86 @@
-// User Login, profile fetch and logout of user functionality created.
-
+// User Login, profile fetch and logout of user functionality created
 const jwt = require("jsonwebtoken");
 const users = require("../data/users");
 
 const CustomError = require("../utils/CustomError");
 
-const JWT_SECRET = require("../middleware/authMiddleware");
+const {JWT_SECRET} = require("../middleware/authMiddleware");
 
-function loginUser(req, res, next) {
-    try {
-        const { email, password } = req.body;
+function loginUser(req,res,next){
+    try{
+        const {email,password} = req.body;
 
         if (!email || !password) {
-            return next(new CustomError("email/password are required.", 400));
+            return next(new CustomError("email/password are required.",400));
         }
-        const user = users.find((u) => u.email === email && u.password === password);
+        const user = users.find((u)=>u.email === email && u.password === password);
 
-        if (!user) {
-            return next(new CustomError("Invalid email/password.", 401));
+        if(!user){
+           return next(new CustomError("Invalid email/password",401));
         }
 
-        const token = jwt.sign({//payload
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role
-        }, JWT_SECRET, { expiresIn: "30m" });//secret key(credentials)
+        const token = jwt.sign({ //payload
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+        },JWT_SECRET,{expiresIn:"30m"}); //secret key(credentials)
 
-        res.cookie("token", token, {//to keep the token data
-            httpOnly: true,
-            secure: false,
-            maxAge: 60 * 60 * 1000
+        res.cookie("token",token,{//to keep the token data 
+            httpOnly:true,
+            secure:false,
+            maxAge:60*60*1000
         });
 
         req.session.user = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            role:user.role
         };
 
         res.status(200).json({ //happy scenario
-            success: true,
-            message: "Login successful",
+            success:true,
+            message:"Login Successful",
             token,
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                role: user.role
+            user:{
+                id:user.id,
+                name:user.name,
+                email:user.email,
+                role:user.role
             }
         });
     }
-    catch (error) {
+    catch(error){
         next(error);
     }
 }
 
-function logoutUser(req, res, next) {
-    try {
-        req.session.destroy(() => {
+function logoutUser(req,res,next){
+    try{
+        req.session.destroy(()=>{
             res.clearCookie("token");
             res.status(200).json({
-                success: true,
-                message: "Logout Successful"
+                success:true,
+                message:"Logout Successful"
             });
         });
     }
-    catch (error) {
+    catch(error){
         next(error);
     }
 }
 
-function getProfile(req, res, next) {
-    try {
-        res.status(200).json({ //happy scenario
-            success: true,
-            message: "Profile fetched Successfully",
+function getProfile(req,res,next){ //happy scenario
+    try{
+        res.status(200).json({
+            success:true,
+            message:"Profile fetched Successfully",
             user:req.user,
             sessionUser: req.session.user || null
         });
     }
-    catch (error) {
+    catch(error){
         next(error);
     }
 }
