452ee71215f5f23de74df96ffd6d57ce23ca6ac6
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-21 12:55:04 +0530

    Created OTP Schema, addedTTL index, created the OTP model, and exported

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/OTP.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        index: true,
+    },
+    otp: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    expiresAt: {
+        type: Date,
+        required: true,
+    },
+    attempts: {
+        type: Number,
+        default: 0,
+    },
+},
+    {
+        timestamps: true,
+    });
+
+    //Time-to-live index(TTL)
+    otpSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+    //Export model
+
+    module.exports = mongoose.model("OTP", otpSchema);
