1fe13fc984a708095ebed034294f76c46e4fe227
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-21 12:03:36 +0530

    created Movie schema, model and exported

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Movie.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Movie title is required"],
+        trim: true,
+        index: true,
+    },
+    genre: {
+        type: String,
+        required: [true, "Genre is required"],
+        enum: ["Action", "Comedy", "Drama", "Horror", "Sci-Fi", "Romance", "Thriller"],
+        index: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: [1, "Rating must be at least 1"],
+        max: [5, "Rating cannot exceed 5"],
+        index: true,
+    },
+    duration: {
+        type: Number,
+        required: [true, "Duration is required"],
+    },
+    releaseDate: {
+        type: Date,
+        required: [true, "Release date is required"],
+        index: true,
+    },
+    poster: {
+        type: String,
+        default: "",
+    },
+    language: {
+        type: String,
+        index: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+},
+    {
+        timestamps: true,
+    });
+
+//Compound index (it indicates that multiple fields are indexed together)
+movieSchema.index({ genre: 1, rating: -1 }); 
+
+// Text index for searching movies by title
+movieSchema.index({ title: 'text' });
+
+//Virtual field
+movieSchema.virtual("isReleased").get(function(){
+    return this.releaseDate <= new Date();
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
