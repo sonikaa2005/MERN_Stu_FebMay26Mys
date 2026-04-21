eb6cd47630c715dc633060d72b4fca7113c1e8d6
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-21 15:00:31 +0530

    Authorization related service function is created

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/auth.service.js
@@ -0,0 +1,49 @@
+const authService = require('../services/auth.service');
+
+//Register
+exports.register = async (req,res,next) => {
+    try{
+        const result = await authService.registerUser(req.body);
+        res.status(201).json({
+            success:true,
+            message:"User registered.OTP sent.",
+            data:result
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+//Verify OTP
+
+exports.verifyOTP = async (req,res,next) => {
+    try{
+        await authService.verifyOTP(req.body);
+
+        res.status(200).json({
+            success:true,
+            message:"OTP verified successfully",
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+//login
+
+exports.login = async (req,res,next) => {
+    try{
+        const result = await authService.loginUser(req.body);
+        
+        res.status(200).json({
+            success:true,
+            message:"Login successful",
+            data:result,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
