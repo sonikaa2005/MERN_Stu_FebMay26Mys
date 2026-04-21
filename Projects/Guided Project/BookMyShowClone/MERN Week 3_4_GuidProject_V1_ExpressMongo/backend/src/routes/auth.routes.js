1959b28c148734da07d90e2a48ad8df3d40b60c4
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-21 15:17:24 +0530

    Routes related to auth is created with request type

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/auth.routes.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/auth.routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
-//Routes
-router.post("/register", authController.register);
-router.post("/verify-otp", authController.verifyOTP);
-router.post("/login", authController.login);
+// Routes
+router.post("/register",authController.register);
+router.post("/verify-otp",authController.verifyOTP);
+router.post("/login",authController.login);
 
 module.exports = router;
