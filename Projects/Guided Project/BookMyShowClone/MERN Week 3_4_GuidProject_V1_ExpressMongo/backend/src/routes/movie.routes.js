0f6f0323b9bc93ee33909d4489998003e354fc24
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-23 10:55:31 +0530

    Linked controller function with respective route/request

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/movie.routes.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/movie.routes.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const movieController = require("../controllers/movie.controller");
+const {protect} = require("../middleware/auth.middleware");
+const {authorize} = require("../middleware/role.middleware");
 
-const { protect } = require("../middleware/auth.middleware");
-const { authorize } = require("../middleware/role.middleware");
+//Public route
+router.get("/",movieController.getMovies);
 
-// public route
-router.get("/", (req, res) => {
-    res.send("Get Movies");
-});
-
-//Admin only route
-router.post("/", protect, authorize("admin"), (req, res) => {
-    res.send("Create Movie");
-});
+// Admin only route
+router.post("/",protect,authorize("admin"),movieController.createMovie);
+router.put("/:id",protect,authorize("admin"),movieController.updateMovie);
+router.delete("/:id",protect,authorize("admin"),movieController.deleteMovie);
 module.exports = router;
