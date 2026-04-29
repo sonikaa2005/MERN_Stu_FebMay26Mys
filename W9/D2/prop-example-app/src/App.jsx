412f74b5a3369566db82ea6b35858c2e86e32b1b
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-29 11:45:31 +0530

    State Lifting

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import { PropBasics } from './components/P1.jsx'
 import { PropDestructuring } from './components/P2.jsx'
-
+import { PropsChildren } from './components/P3.jsx'
+import { PropDrillingDemo } from './components/P4.jsx'
+import { SharedStateParent } from './components/P5.jsx'
 function App() {
 
   return (
     <>
     {/* <PropBasics/> */}
-    <PropDestructuring/>
+    {/* <PropDestructuring/> */}
+    {/* <PropsChildren /> */}
+    {/* <PropDrillingDemo/> */}
+    <SharedStateParent/>
     </>
   )
 }
