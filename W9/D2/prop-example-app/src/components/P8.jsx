b11410694ebc779c4c2a12b15e2e04f3666c7ed7
sonikaa2005 <sonikagowda974@gmail.com>
2026-04-29 13:49:41 +0530

    Props immutabile

---
+++ W9/D2/prop-example-app/src/components/P8.jsx
@@ -0,0 +1,17 @@
+//Props immutabile
+//Notification count
+function Child({message}){
+    
+    //message = 'Changed message';
+    return <p>Received message: {message}</p>
+
+}
+export function PropsImmutability(){
+    const parentMessage = "Props are read-only";
+    return(
+        <>
+            <h2>Props are immutable.</h2>
+            <Child message={parentMessage} />
+        </>
+    )
+}
