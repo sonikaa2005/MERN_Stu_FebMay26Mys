f00d3a03a3aacea2393ce289488ee14db59eaf17
sonikaa2005 <sonikagowda974@gmail.com>
2026-05-04 12:53:59 +0530

    app.jsx

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 import { UseStateBasics } from './components/P1'
+import { UpdatingObjectsArraysState } from './components/P2'
+import { EventHandlingState } from './components/P3'
+import { DerivedStateCondRender } from './components/P4'
 
 function App() {
     return(
       <>
-       <UseStateBasics />
+       {/* <UseStateBasics /> */}
+       {/* <UpdatingObjectsArraysState /> */}
+       {/* <EventHandlingState /> */}
+       <DerivedStateCondRender />
       </>
     )
 }
