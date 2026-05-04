058fcafd95e0a7e235f1451a26aa81d89eafe5c9
sonikaa2005 <sonikagowda974@gmail.com>
2026-05-04 11:15:15 +0530

    Authorization related service function is created

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/auth.service.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/auth.service.js
@@ -1,70 +1,61 @@
+// src\services\auth.service.js
 const User = require("../models/User");
 const OTP = require("../models/OTP");
 const otpService = require("./otp.service");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const CustomError = require("../utils/customError");
 
-// Register user
+//REGISTER USER
 exports.registerUser = async ({ name, email, password }) => {
     const existingUser = await User.findOne({ email });
-
     if (existingUser) {
-        throw new Error("User already exists");
+        throw new CustomError("User already exists", 409);
     }
-
     const user = await User.create({
         name,
         email,
         password,
     });
-
     await otpService.generateOTP(email);
-
     return { email: user.email };
 };
 
-//Verify OTP
+//VERIFY OTP
 exports.verifyOTP = async ({ email, otp }) => {
     const record = await OTP.findOne({ email }).select("+otp");
-
     if (!record) {
-        throw new Error("OTP expired or not found");
+        throw new CustomError("OTP expired or not found", 410);
     }
-
     const isMatch = await bcrypt.compare(otp, record.otp);
-
     if (!isMatch) {
         record.attempts += 1;
         await record.save();
-        throw new Error("Invalid OTP");
-    }
 
+        throw new CustomError("Invalid OTP", 400);
+    }
     await User.updateOne({ email }, { isVerified: true });
     return true;
 };
 
-// Login
+//LOGIN USER
 exports.loginUser = async ({ email, password }) => {
     const user = await User.findOne({ email }).select("+password");
-
     if (!user) {
-        throw new Error("User not found");
+        throw new CustomError("User not found", 400);
     }
     if (!user.isVerified) {
-        throw new Error("User not verified");
+        throw new CustomError("User not verified", 403);
     }
     const isMatch = await user.comparePassword(password);
-
     if (!isMatch) {
-        throw new Error("Invalid credentials");
+        throw new CustomError("Invalid credentials", 401);
     }
-
     const token = jwt.sign(
         { id: user._id, role: user.role },
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
     );
-
     return {
         token,
         user: {
