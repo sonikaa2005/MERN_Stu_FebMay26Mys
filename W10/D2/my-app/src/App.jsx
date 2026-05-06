2875b17d3ff149006a7c8c39b7fcf5eeab47ead9
sonikaa2005 <sonikagowda974@gmail.com>
2026-05-06 12:59:39 +0530

    useRef Hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { UseContextIntro } from './components/P1'
 import { CustomHooksIntro } from './components/P2'
+import { UserRefIntro } from './components/P3'
 
 function App() {
 
   return (
     <>
     {/* <UseContextIntro/> */}
-    <CustomHooksIntro/>
+    {/* <CustomHooksIntro/> */}
+    <UserRefIntro />
     </>
 
   )
