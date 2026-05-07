e032431a5a5f89b3757c16bd0ec5b93d2522402e
sonikaa2005 <sonikagowda974@gmail.com>
2026-05-07 15:47:48 +0530

    Stop event propagation

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -7,6 +7,7 @@ import { UseCallbackEvents } from './components/P5'
 import { EventDelegation } from './components/P6'
 import { FormsPattern } from './components/P7'
 import { KeyboardEvent } from './components/P8'
+import { EventObj } from './components/P9'
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
       {/* <UseCallbackEvents/> */}
       {/* <EventDelegation /> */}
       {/* <FormsPattern /> */}
-      <KeyboardEvent />
+      {/* <KeyboardEvent /> */}
+      <EventObj/>
       
     </>
   )
