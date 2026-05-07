d70eb1fc6ad1a92ba0bd0a869487ac5e6346acc8
sonikaa2005 <sonikagowda974@gmail.com>
2026-05-07 12:32:58 +0530

    useCallback with Event Handlers

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import { EventBasics } from './components/P1'
 import { EventObject } from './components/P2'
 import { PassingArguments } from './components/P3'
+import { UseCallbackEvents } from './components/P5'
 
 function App() {
   return (
     <>
       {/* <EventBasics /> */}
       {/* <EventObject /> */}
-      <PassingArguments/>
+      {/* <PassingArguments/> */}
+      <UseCallbackEvents/>
     </>
   )
 }
