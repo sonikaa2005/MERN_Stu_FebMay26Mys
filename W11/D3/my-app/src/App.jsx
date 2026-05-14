5c3e02e3383ab52fd69264e95f88edd8a4df8b0b
sonikaa2005 <sonikagowda974@gmail.com>
2026-05-14 12:19:47 +0530

    Error boundary & file upload progress

--- W11/D3/my-app/src/App.jsx
+++ W11/D3/my-app/src/App.jsx
@@ -4,6 +4,7 @@ import { UseEffectLifeCycle } from './components/P2';
 import { AxiosLifecycle } from './components/P3';
 import { CRUDOperations } from './components/P4';
 import { PaginationCaching } from './components/P5';
+import { UploadErrorBoundary } from './components/P6';
 function App() {
 
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <UseEffectLifeCycle /> */}
       {/* <AxiosLifecycle/> */}
       {/* <CRUDOperations /> */}
-      <PaginationCaching/>
+      {/* <PaginationCaching/> */}
+      <UploadErrorBoundary />
     </>
   )
 }
