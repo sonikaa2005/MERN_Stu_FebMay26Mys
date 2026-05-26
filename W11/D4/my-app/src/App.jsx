31a286bdcf8632bc46a61dbeb083bae5730fc901
sonikaa2005 <sonikagowda974@gmail.com>
2026-05-26 13:28:21 +0530

    controlled, validation & async form

--- W11/D4/my-app/src/App.jsx
+++ W11/D4/my-app/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { ControlledForm } from './components/P1'
+import { ValidationForm } from './components/P2'
+import { AsyncForm } from './components/P3'
 function App() {
 
   return (
     <>
-      < ControlledForm/>
+      {/* < ControlledForm/> */}
+      {/* < ValidationForm /> */}
+      <AsyncForm />
     </>
   )
 }
