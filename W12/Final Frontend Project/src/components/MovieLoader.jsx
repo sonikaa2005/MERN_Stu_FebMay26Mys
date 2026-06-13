1980aa84d0b13badfd23bfe67c0aa4c7bbad54b4
sonikaa2005 <sonikagowda974@gmail.com>
2026-06-13 12:24:04 +0530

    Conditional Rendering

---
+++ W12/Final Frontend Project/src/components/MovieLoader.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+export default function MovieLoader(){
+    const [isLoading, setIsLoading] = useState(true);
+
+    return(
+        <section>
+            <h2>Movies</h2>
+            {isLoading ? <p>Loading Movies...</p>: <p> Movie Loaded successfully</p>}
+            <button onClick={() => {setIsLoading(!isLoading);}}> Toggle Laoding</button>
+        </section>
+    )
+}
---
+++ W12/Final Frontend Project/src/components/MovieResults.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function MovieResults() {
+    const [movies, setMovies] = useState([]);
+
+    return (
+        <section>
+            <h2>Search results</h2>
+            {movies.length === 0 ? <p>No Movies Found</p> :
+                <ul>
+                    {movies.map((movie)=>(<li key={movie}>{movie}</li>))}
+                </ul>}
+                <button onClick={()=>{
+                    setMovies(["Inception", "Dhurandar", "xyz"]);
+                }}>Load Movies</button>
+        </section>
+    )
+}
