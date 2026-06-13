7fab86afadf40bc32183c281aeb9e97d0be5c671
sonikaa2005 <sonikagowda974@gmail.com>
2026-06-13 12:22:55 +0530

    Event, Event Object and Event handling

---
+++ W12/Final Frontend Project/src/components/SearchShortcut.jsx
@@ -0,0 +1,19 @@
+
+
+export default function SearchShortcut(params) {
+    function handleKeyDown(){
+        if(event.key === "Enter"){
+            alert("Search Initiated");
+        }
+        if(event.key === "Escape"){
+            alert("Search cleared")
+        }
+    }
+    return(
+        <section>
+            <h2>Keyboard Search</h2>
+            <input type="text" placeholder="Press Enter"
+            onKeyDown={handleKeyDown}/>
+        </section>
+    );
+}
