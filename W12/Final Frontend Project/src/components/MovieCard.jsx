b7082ea44837c79a29c65386cb7c25e1e42f5474
sonikaa2005 <sonikagowda974@gmail.com>
2026-06-13 12:23:29 +0530

    Usage of Props

--- W12/Final Frontend Project/src/components/MovieCard.jsx
+++ W12/Final Frontend Project/src/components/MovieCard.jsx
@@ -1,19 +1,31 @@
-export default function MovieCard(){
-    const movie = {
-        title: "Inception",
-        genre: "Sci-Fi",
-        rating: 4.8,
-        duration: "2h 28m"
-    };
+export default function MovieCard(props){
+    // const movie = {
+    //     title: "Inception",
+    //     genre: "Sci-Fi",
+    //     rating: 4.8,
+    //     duration: "2h 28m"
+    // };
+
+    //return(
+//         <div className="movie-card">
+//             <img src="https://picsum.photos/200/300" alt={movie.title} />
+//             <h2>{movie.title}</h2>
+//             <p>Genre: {movie.genre}</p>
+//             <p>Rating: {movie.rating}</p>
+//             <p>Duration: {movie.duration}</p>
+//             <button>Book Now</button>
+//         </div>
+//     )
+// }
 
     return(
         <div className="movie-card">
-            <img src="https://picsum.photos/200/300" alt={movie.title} />
-            <h2>{movie.title}</h2>
-            <p>Genre: {movie.genre}</p>
-            <p>Rating: {movie.rating}</p>
-            <p>Duration: {movie.duration}</p>
+            <img src={props.poster} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>Genre: {props.genre}</p>
+            <p>Rating: {props.rating}</p>
+            <p>Duration: {props.duration}</p>
             <button>Book Now</button>
         </div>
-    )
+    );
 }
