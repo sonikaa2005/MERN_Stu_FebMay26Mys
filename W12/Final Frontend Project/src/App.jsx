bf06465ade9fc15bc77c0a44c8fa2c5ee4a65d95
sonikaa2005 <sonikagowda974@gmail.com>
2026-06-13 15:38:49 +0530

    Error Boundary, Pagination, filtering

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -11,124 +11,126 @@ import './App.css'
 // import MovieLoader from './components/MovieLoader';
 // import MovieResults from './components/MovieResults';
 
-import { Routes, Route, useParams, useNavigate, Navigate, Link } from "react-router-dom";
-import Navbar from './components/Navbar';
-import DashboardSection from './components/DashboardSection';
+//import { Routes, Route, useParams, useNavigate, Navigate, Link } from "react-router-dom";
+//import Navbar from './components/Navbar';
+//import DashboardSection from './components/DashboardSection';
+import MovieExplorer from './components/MovieExplorer';
+import ErrorBoundary from './components/ErrorBoundary';
 
-const isAuthenticated = true;
-function ProtectedRoute({ children }) {
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />
-  }
-  return children;
-}
+// const isAuthenticated = true;
+// function ProtectedRoute({ children }) {
+//   if (!isAuthenticated) {
+//     return <Navigate to="/login" replace />
+//   }
+//   return children;
+// }
 
-function HomePage() {
-  return (
-    <section>
-      <h2>Home</h2>
-      <p>Welcome to Bookmyshow</p>
-    </section>
-  );
-}
+// function HomePage() {
+//   return (
+//     <section>
+//       <h2>Home</h2>
+//       <p>Welcome to Bookmyshow</p>
+//     </section>
+//   );
+// }
 
-function MoviePage() {
-  const navigate = useNavigate();
-  const movies = [
-    {
-      id: 101,
-      title: "Inception"
-    },
-    {
-      id: 102,
-      title: "Interstellar"
-    },
-  ];
-  return (
-    <section>
-      <h2>Movies</h2>
-      <p>Click a movie to view details</p>
-      <ul>
-        {movies.map((movie)=>{
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        })}
-      </ul>
-    </section>
-  );
-}
-function MovieDetailsPage() {
-  const { movieId } = useParams();
-  return (
-    <section>
-      <h3>Movie Details</h3>
-      <p>Movie ID: {movieId}</p>
-    </section>
-  );
-}
+// function MoviePage() {
+//   const navigate = useNavigate();
+//   const movies = [
+//     {
+//       id: 101,
+//       title: "Inception"
+//     },
+//     {
+//       id: 102,
+//       title: "Interstellar"
+//     },
+//   ];
+//   return (
+//     <section>
+//       <h2>Movies</h2>
+//       <p>Click a movie to view details</p>
+//       <ul>
+//         {movies.map((movie)=>{
+//           <li key={movie.id}>
+//             <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+//           </li>
+//         })}
+//       </ul>
+//     </section>
+//   );
+// }
+// function MovieDetailsPage() {
+//   const { movieId } = useParams();
+//   return (
+//     <section>
+//       <h3>Movie Details</h3>
+//       <p>Movie ID: {movieId}</p>
+//     </section>
+//   );
+// }
 
-function LoginPage() {
-  const navigate = useNavigate();
-  return (
-    <section>
-      <h2>Login</h2>
-      <p>Login Screen</p>
-      <button
-        style={styles.button}
-        onClick={() => {
-          navigate("/bookings");
-        }}>
-        Login
-      </button>
-    </section>
-  );
-}
+// function LoginPage() {
+//   const navigate = useNavigate();
+//   return (
+//     <section>
+//       <h2>Login</h2>
+//       <p>Login Screen</p>
+//       <button
+//         style={styles.button}
+//         onClick={() => {
+//           navigate("/bookings");
+//         }}>
+//         Login
+//       </button>
+//     </section>
+//   );
+// }
 
-function SignupPage() {
-  return (
-    <section>
-      <h2>Signup</h2>
-      <p>New users can register here.</p>
-    </section>
-  );
-}
+// function SignupPage() {
+//   return (
+//     <section>
+//       <h2>Signup</h2>
+//       <p>New users can register here.</p>
+//     </section>
+//   );
+// }
 
-function BookingsPage() {
-  return (
-    <section>
-      <h2>My Bookings</h2>
-      <p>Protected Page.</p>
-    </section>
-  );
-}
+// function BookingsPage() {
+//   return (
+//     <section>
+//       <h2>My Bookings</h2>
+//       <p>Protected Page.</p>
+//     </section>
+//   );
+// }
 
-function DashboardOverview() {
-  return (
-    <section>
-      <h2>Dashboard Overview</h2>
-      <p>Booking stats.</p>
-    </section>
-  );
-}
+// function DashboardOverview() {
+//   return (
+//     <section>
+//       <h2>Dashboard Overview</h2>
+//       <p>Booking stats.</p>
+//     </section>
+//   );
+// }
 
-function DashboardMovies() {
-  return (
-    <section>
-      <h2>Dashboard Movies</h2>
-      <p>Managed movies here</p>
-    </section>
-  );
-}
+// function DashboardMovies() {
+//   return (
+//     <section>
+//       <h2>Dashboard Movies</h2>
+//       <p>Managed movies here</p>
+//     </section>
+//   );
+// }
 
-function NotFoundPage() {
-  return (
-    <section>
-      <h2>404</h2>
-      <p>Page not found.</p>
-    </section>
-  );
-}
+// function NotFoundPage() {
+//   return (
+//     <section>
+//       <h2>404</h2>
+//       <p>Page not found.</p>
+//     </section>
+//   );
+// }
 
 //function App() {
 //return (
@@ -174,44 +176,59 @@ function NotFoundPage() {
 
 //);
 //}
-export default function App() {
-  return (
-    <div style={styles.container}>
-      <Navbar />
-      <Routes>
-        {/* public routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviePage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+// export default function App() {
+//   return (
+//     <div style={styles.container}>
+//       <Navbar />
+//       <Routes>
+//         {/* public routes */}
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/movies" element={<MoviePage />} />
+//         <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/signup" element={<SignupPage />} />
 
-        {/* protected routes */}
-        <Route path="/bookings"
-          element={
-            <ProtectedRoute>
-              <BookingsPage />
-            </ProtectedRoute>
-          } />
+//         {/* protected routes */}
+//         <Route path="/bookings"
+//           element={
+//             <ProtectedRoute>
+//               <BookingsPage />
+//             </ProtectedRoute>
+//           } />
 
-          {/* Nested Dashboard routes */}
-          <Route path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <DashboardSection />
-            </ProtectedRoute>
-          } />
+//           {/* Nested Dashboard routes */}
+//           <Route path="/dashboard"
+//           element={
+//             <ProtectedRoute>
+//               <DashboardSection />
+//             </ProtectedRoute>
+//           } />
 
-          {/* Index route */}
-          <Route index element={<DashboardOverview />} />
-          <Route path="movies" element={<DashboardMovies />} />
-          {/* <Route path="shows" element={<DashboardShows />} /> */}
+//           {/* Index route */}
+//           <Route index element={<DashboardOverview />} />
+//           <Route path="movies" element={<DashboardMovies />} />
+//           {/* <Route path="shows" element={<DashboardShows />} /> */}
 
-          {/* 404 route */}
-          <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+//           {/* 404 route */}
+//           <Route path="*" element={<NotFoundPage />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+
+// it is for MovieExplore and ErrorBoundary
+
+export default function App(){
+  return(
+    <div>
+      <header>
+        <ErrorBoundary>
+          <MovieExplorer />
+        </ErrorBoundary>
+      </header>
     </div>
-  );
+  )
 }
 
 const styles = {
